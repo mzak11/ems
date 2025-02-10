5be46f19f7b440cd3e657b9a34c74a2cf7a600a1
mzak11 <mzakwan184@gmail.com>
2025-02-11 00:51:50 +0530

    render dashboard component

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ const App = () => {
 
   return (
     <>
-      <Login />
-      {/* <EmployeeDashboard/> */}
+      {/* <Login /> */}
+      <EmployeeDashboard/>
       {/* <AdminDashboard/> */}
     </>
   );
